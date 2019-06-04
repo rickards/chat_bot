4a58aa46c68de58a963d692b75dbef8528440f10
Matheus Moura <matheus.moura13@gmail.com>
2019-06-03 22:18:57 -0300

    Fixa view do input de texto

--- pwa/src/pages/Chat/index.tsx
+++ pwa/src/pages/Chat/index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import LayoutDrawer from "../../components/LayoutDrawer";
-import { Grid, Button, TextField, Paper, InputBase, IconButton, Divider, Icon } from "@material-ui/core";
+import { Grid, Paper, InputBase, IconButton, Divider } from "@material-ui/core";
 import SendIcon from "@material-ui/icons/Send";
 
 import styles from "./Chat.scss";
@@ -10,15 +10,27 @@ export default class Chat extends Component {
   public render() {
     return (
       <LayoutDrawer title="Chicó - Home">
-        <Grid container spacing={3} className={styles.footer}>
-
-          <InputBase className={styles.input} placeholder="Digite sua mensagem aqui" />
-          <IconButton color="primary" className={styles.iconButton} aria-label="Directions">
-            <SendIcon />
-          </IconButton>
-
-        </Grid>
-      </LayoutDrawer >
+        {/* Rodapé com input de texto */}
+        <div className={styles.footer}>
+          <Grid container spacing={3} justify="center">
+            <Grid item xs={12}>
+              <Paper className={styles.paper}>
+                <InputBase
+                  className={styles.input}
+                  placeholder="Digite sua mensagem aqui"
+                />
+                <IconButton
+                  color="primary"
+                  className={styles.iconButton}
+                  aria-label="Directions"
+                >
+                  <SendIcon />
+                </IconButton>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
+      </LayoutDrawer>
     );
   }
 }
